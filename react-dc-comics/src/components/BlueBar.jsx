1065066e807e33b1e297ac3516f403eb7a68f70a
dannyarena <arenadanny71@gmail.com>
2025-05-20 20:52:12 +0200

    add style to bluebar items

--- react-dc-comics/src/components/BlueBar.jsx
+++ react-dc-comics/src/components/BlueBar.jsx
@@ -1,23 +1,23 @@
 function BlueBar () {
     return (
         <section className="blue-bar">
-            <div className="bar.item">
+            <div className="bar-item">
                 <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" />
                 <span>DIGITAL COMICS</span>
             </div>
-            <div className="bar.item">
+            <div className="bar-item">
                 <img src="/img/buy-comics-merchandise.png" alt="DC Merchandise" />
                 <span>DC MERCHANDISE</span>
             </div>
-            <div className="bar.item">
+            <div className="bar-item">
                 <img src="/img/buy-comics-shop-locator.png" alt="Subscription" />
                 <span>SUBSCRIPTION</span>
             </div>
-            <div className="bar.item">
+            <div className="bar-item">
                 <img src="/img/buy-comics-subscriptions.png" alt="Comic Shop Locator" />
                 <span>COMIC SHOP LOCATOR</span>
             </div>
-            <div className="bar.item">
+            <div className="bar-item">
                 <img src="/img/buy-dc-power-visa.svg" alt="DC Power Visa" />
                 <span>DC POWER VISA</span>
             </div>
