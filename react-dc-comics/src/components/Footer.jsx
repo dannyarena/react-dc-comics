154a9f823a292bf10dc6547f43bc2f4d8d02bbd7
dannyarena <arenadanny71@gmail.com>
2025-05-20 21:21:21 +0200

    add footer top elements and structure

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -1,7 +1,57 @@
 function Footer () {
     return (
-        <footer>
-
+        <footer className="footer">
+            <div className="footer-top">
+                <div className="links.container">
+                    <div className="link-column">
+                        <h4>DC COMICS</h4>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <h4>SHOP</h4>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+                    <h4>DC</h4>
+                    <div className="link-column">
+                    <ul>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                        <li>Terms Of Use</li>
+                    </ul>
+                </div>
+                <div className="link-column">
+                    <h4>SITES</h4>
+                    <ul>
+                        <li>DC</li>
+                        <li>DC</li>
+                        <li>DC</li>
+                        <li>DC</li>
+                        <li>DC</li>
+                    </ul>
+                </div>
+                </div>
+                <div className="footer-logo-bg">
+                    <img src="/img/dc-logo-bg.png" alt="DC Logo Background" />
+                </div>
+            </div>
+            
         </footer>
     );
 }
