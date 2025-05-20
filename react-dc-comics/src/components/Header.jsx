74aadbf1ee739e996297f7975b7a990440c93f75
dannyarena <arenadanny71@gmail.com>
2025-05-20 19:21:27 +0200

    creating header structure and add logo dc

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,7 +1,28 @@
 function Header () {
     return (
         <header>
+            <div className="container">
+                {/* {logo} */}
+                <div className="logo">
+                    <img src="/img/dc-logo.png" alt="DC logo" />
+                </div>
+            </div>
 
+            {/* Menu di navigazione */}
+            <nav>
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                    <li>CHARACTERS</li>
+                </ul>
+            </nav>
         </header>
     );
 }
