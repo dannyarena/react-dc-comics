fbe5244168dce07503733d837d89f06e2b661417
dannyarena <arenadanny71@gmail.com>
2025-05-20 19:48:44 +0200

    add styile to header and container

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import BlueBar from './components/BlueBar';
@@ -5,14 +6,12 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
       <>
       <Header />
       <MainContent />
       <BlueBar />
       <Footer />
       </>
-    </div>
   );
 }
 
--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -12,15 +12,15 @@ function Header () {
             <nav>
                 <ul>
                     <li>CHARACTERS</li>
-                    <li>CHARACTERS</li>
-                    <li>CHARACTERS</li>
-                    <li>CHARACTERS</li>
-                    <li>CHARACTERS</li>
-                    <li>CHARACTERS</li>
-                    <li>CHARACTERS</li>
-                    <li>CHARACTERS</li>
-                    <li>CHARACTERS</li>
-                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
                 </ul>
             </nav>
         </header>
