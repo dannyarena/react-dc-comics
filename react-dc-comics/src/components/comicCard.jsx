ca30619abd09c2d062cff0b897623cf1e1c0113c
dannyarena <arenadanny71@gmail.com>
2025-05-22 16:14:05 +0200

    add function for comics card dinamic creation

--- react-dc-comics/src/components/comicCard.jsx
+++ react-dc-comics/src/components/comicCard.jsx
@@ -1,3 +1,11 @@
 function comicCard(props) {
-    
+    return (
+        <div className="comic-card">
+            <img src={props.thumb} alt={props.title} />
+            <h3>{props.title}</h3>
+
+        </div>
+    );
 }
+
+export default comicCard;
